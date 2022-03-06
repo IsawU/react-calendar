d8edda6aef6ea6073fae68a46c10b079cab5fa33
IsawU <17961938+IsawU@users.noreply.github.com>
2022-03-06 16:39:49 +0100

    Add month controls

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,48 @@ import Calendar from './Calendar';
 type AppProps = {};
 
 type AppState = {
-  date: Date;
+  today: Date;
+  month: Date;
 }
 
 export default class App extends React.Component<AppProps, AppState> {
   constructor (props: {}) {
     super(props);
     this.state = {
-      date: new Date(),
+      today: new Date(),
+      month: new Date(),
     };
   }
 
   render() {
-    const change: React.MouseEventHandler<HTMLButtonElement> = () => {
-      const date = new Date(this.state.date);
-      date.setDate(date.getDate()-28);
-      this.setState({date: date});
-      console.log(`changed date ${date}`);
+    const monthDown: React.MouseEventHandler<HTMLButtonElement> = () => {
+      const date = new Date(this.state.month);
+      date.setDate(0);
+      this.setState({month: date});
+    }
+
+    const monthUp: React.MouseEventHandler<HTMLButtonElement> = () => {
+      const date = new Date(this.state.month);
+      date.setDate(32);
+      this.setState({month: date});
+    }
+
+    const monthToday: React.MouseEventHandler<HTMLButtonElement> = () => {
+      const date = new Date(this.state.today);
+      this.setState({month: date});
     }
 
     return (
       <div className={styles.contentStrip}>
-        <button onClick={change}>change</button>
-        <Calendar today={this.state.date} month={this.state.date}/>
+        <div className={styles.controls}>
+          <button onClick={monthDown}>&lt;</button>
+          <div className={styles.month}>
+            {this.state.month.toLocaleDateString(navigator.language, { month: 'long' })}
+          </div>
+          <button onClick={monthUp}>&gt;</button>
+          <button onClick={monthToday}>Today</button>
+        </div>
+        <Calendar today={this.state.today} month={this.state.month}/>
       </div>
     )
   }
