c15dfb9c3abdde5665bfefbd0fc92a4bd02c808b
Jiří Jureček <jurecek@divelit.cz>
2022-03-08 10:07:41 +0100

    Show year under month

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -10,6 +10,10 @@ export function getLongMonth(date: Date): string {
   return date.toLocaleDateString(navigator.language, { month: 'long' });
 }
 
+export function getLongYear(date: Date): string {
+  return date.toLocaleDateString(navigator.language, { year: 'numeric' });
+}
+
 export function getHumanReadableDate(date: Date): string {
   return date.toLocaleDateString(navigator.language, {  year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: false });
 }
