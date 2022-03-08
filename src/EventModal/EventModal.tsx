1ce1b35327e299bc5cf41969ba715cbd0b3c6ee0
IsawU <17961938+IsawU@users.noreply.github.com>
2022-03-08 10:51:51 +0100

    EventModal date placeholders

--- src/EventModal/EventModal.tsx
+++ src/EventModal/EventModal.tsx
@@ -165,9 +165,9 @@ function eventModal(props: EventProps): JSX.Element {
           </div>
           <div className={styles.container}>
             <div className={styles.label}>From:</div>
-            <input type="text" className={getValidatedStyle(from)} value={from.value} onChange={fromChange}/>
+            <input type="text" className={getValidatedStyle(from)} value={from.value} onChange={fromChange} placeholder={getHumanReadableDate(props.event.from)}/>
             <div className={styles.label}>To:</div>
-            <input type="text" className={getValidatedStyle(to)} value={to.value} onChange={toChange}/>
+            <input type="text" className={getValidatedStyle(to)} value={to.value} onChange={toChange} placeholder={getHumanReadableDate(props.event.to)}/>
             <div className={styles.label}>Color:</div>
             <input type="text" value={color} onChange={colorChange} placeholder="#000000 format color"/>
           </div>
