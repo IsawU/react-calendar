d02e87c169ce5a5dc3e16d8de3e5e4281e16c25a
IsawU <17961938+IsawU@users.noreply.github.com>
2022-03-08 17:13:00 +0100

    Update getTextColor

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -23,23 +23,39 @@ export function getDefaultEventColor(): string {
   return '#add8e6';
 }
 
+function hexColorToRGB(hex: string): {r: number, g: number, b: number} {
+  if (hex.startsWith('#')) {
+    if (hex.length == 4) {
+      const rs = hex.substring(1, 2);
+      const gs = hex.substring(2, 3);
+      const bs = hex.substring(3, 4);
+
+      const r = parseInt(rs, 16) * 0x11;
+      const g = parseInt(gs, 16) * 0x11;
+      const b = parseInt(bs, 16) * 0x11;
+      return { r: r, g: g, b: b };
+    }
+    else if (hex.length == 7) {
+      const rs = hex.substring(1, 3);
+      const gs = hex.substring(3, 5);
+      const bs = hex.substring(5, 7);
+
+      const r = parseInt(rs, 16);
+      const g = parseInt(gs, 16);
+      const b = parseInt(bs, 16);
+      return { r: r, g: g, b: b };
+    }
+  }
+  return { r: 173, g: 216, b: 230 };
+}
+
 export function getTextColor(backgroundColor: string): string {
   // TODO: There's a lot of room to improve here. This is very basic.
-  if (backgroundColor.startsWith('#')) {
-    const rs = backgroundColor.substring(1, 3);
-    const gs = backgroundColor.substring(3, 5);
-    const bs = backgroundColor.substring(5, 7);
-
-    if (rs.length == 2 && gs.length == 2 && bs.length == 2) {
-      const r = parseInt(rs, 16);
-      const g = parseInt(rs, 16);
-      const b = parseInt(rs, 16);
-      // https://www.w3.org/TR/AERT/#color-contrast
-      const brightness = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-      if (isFinite(brightness)) {
-        return brightness > 125 ? 'black' : 'white';
-      }
-    }
+  const color = hexColorToRGB(backgroundColor);
+  // https://www.w3.org/TR/AERT/#color-contrast
+  const brightness = ((color.r * 299) + (color.g * 587) + (color.b * 114)) / 1000;
+  if (isFinite(brightness)) {
+    return brightness > 125 ? 'black' : 'white';
   }
   return 'black';
 }
